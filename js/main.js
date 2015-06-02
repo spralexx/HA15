1c3055681c884f4ccdeeb4e0334ce444f9f3a002
spralexx <rene.krick@gmx.de>
2015-06-02 16:15:19 +0200

    added random colors for menue on left side

--- js/main.js
+++ js/main.js
@@ -42,16 +42,35 @@ function prepareDrawPane() {
 
 function PrepareColorChooser(counter){
 
+	var colorArray=returnColorArray(counter);
+	console.log(colorArray);
+	var colorCounter=colorArray.length-1;
+
   for(var i=0;i<counter;i++){
     this.tr=document.createElement("tr");
     this.tr.setAttribute("id","tr"+i);
     for(var x=0;x<counter;x++){
       this.td=document.createElement("td");
       this.td.setAttribute("id","td"+i+"_"+x);
+			this.td.setAttribute("style","background-color: "+getNextColor()+";"); //set color from color set
+			this.space=document.createElement("br");
+			this.td.appendChild(this.space);
       this.tr.appendChild(this.td);
     }
     document.getElementById("toolsAndColors").appendChild(this.tr);
   }
+
+
+	function getNextColor(){
+		return colorArray[colorCounter--];
+	}
+	function returnColorArray(size){
+		var array=[];
+		for(var i=0;i<(size*size);i++){
+				array[i]="rgb("+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+","+Math.round(Math.random()*255)+")";
+			}
+			return array;
+	}
 
 }
 
